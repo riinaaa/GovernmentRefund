a9806adf2323d20d4e4596135bd356742fb8e1d3
Khad1jah <Khadijaha.zh@gmail.com>
2021-07-06 02:18:04 +0300

    Pax Report Updated

--- GovernmentRefund/PaxReport.aspx
+++ GovernmentRefund/PaxReport.aspx
@@ -1,4 +1,4 @@
-﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Paxm.Master" AutoEventWireup="true" CodeFile="PaxReport.aspx.cs" Inherits="GovernmentRefund.PaxReport" %>
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Paxm.Master" AutoEventWireup="true" CodeBehind="PaxReport.aspx.cs" Inherits="GovernmentRefund.PaxReport" %>
 <asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
 
     <div class="text-center">
@@ -15,15 +15,17 @@
                 </td>
             </tr>
         </table>
-             <asp:Label ID="Label1" runat="server" Text="From:"></asp:Label>
+        <asp:Label ID="Label1" runat="server" Text="From:"></asp:Label>
          <asp:TextBox ID="TxtDob" runat="server" TextMode="Date" style="color:black"> </asp:TextBox>
         <asp:Label ID="Label2" runat="server" Text="To:"></asp:Label>
          <asp:TextBox ID="TxtDob2" runat="server" TextMode="Date" style="color:black"> </asp:TextBox>
+
+
  <asp:Button ID="Button1" runat="server" Height="39px" Text="Search" Width="123px" CssClass="my-button" OnClick="Button1_Click" />
-
         <br />
+        
 
-       <asp:DropDownList id="DropDownList1" runat="server" style="color:black" AutoPostBack="true">
+          <asp:DropDownList id="DropDownList1" runat="server" style="color:black" AutoPostBack="true">
                <asp:ListItem Value="All" Text="All" /> 
                <asp:ListItem Value="Approved" Text="Approved" />
                <asp:ListItem Value="Rejected" Text="Rejected" />
@@ -32,36 +34,14 @@
 
     </div>   <br />
 
-    <table style="border-color: #CCCCCC; width: 100%; height: 100px; background-color: #eee7dd;" border="1">
-        <tr>
-            <td>&nbsp; Request ID </td>
-            <td>&nbsp; Request Date</td>
-            <td>&nbsp; By</td>
-            <td>&nbsp; Total Amount</td>
-            <td>&nbsp; Tax K7 </td>
-            <td>&nbsp; Sundry Charges </td>
-            <td>&nbsp; Refernce Number</td>
-            <td>&nbsp; Account Number</td>
-            <td>&nbsp; Status</td>
-        </tr>
-        <tr>
-            <td class="auto-style2">&nbsp; 101 </td>
-            <td>&nbsp; june 23th 2021</td>
-            <td>&nbsp; Rina </td>
-            <td>&nbsp; 2000.00</td>
-            <td>&nbsp; 300.00 </td>
-            <td>&nbsp; 0.00 </td>
-            <td>&nbsp; 2928383 </td>
-            <td>&nbsp; 102938474939</td>
-            <td>&nbsp; Processing</td>
-        </tr>
-    </table>
+        <asp:GridView ID="GridView1" class="table table-bordered table-condensed table-responsive table-hover" HeaderStyle-BackColor="#eee7dd" runat="server" BorderColor="White" ForeColor="Black">
+        </asp:GridView>
+
+
     <h4>Total Requests: </h4>
     <asp:Button ID="Button2" runat="server" Height="47px" Text="Generate Report" Width="151px" CssClass="my-button" OnClick="Button2_Click" />
 
 
 
-
 </div>
-
 </asp:Content>
--- GovernmentRefund/PaxReport.aspx.cs
+++ GovernmentRefund/PaxReport.aspx.cs
@@ -1,27 +1,127 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.IO;
 
 namespace GovernmentRefund
 {
     public partial class PaxReport : System.Web.UI.Page
     {
+        SqlConnection con = new SqlConnection(@"Data Source=RINA-RAZER\SQLEXPRESS;Initial Catalog=GR;Integrated Security=True");
+
         protected void Page_Load(object sender, EventArgs e)
         {
+            if (con.State == ConnectionState.Open)
+            {
+                con.Close();
+            }
+            con.Open();
+            String status = DropDownList1.SelectedValue;
+
+            if (status.Equals("All"))
+            {
+                displayData();
+            }
+            else if (status.Equals("Approved"))
+            {
+                displayData(status);
+            }
+            else if (status.Equals("Rejected"))
+            {
+                displayData(status);
+            }
+
+            else if (status.Equals("In Progress"))
+            {
+                displayData(status);
+            }
+
 
         }
+
         protected void Button1_Click(object sender, EventArgs e)
         {
-            Response.Redirect("PaxReport.aspx");
-
+            String from = TxtDob.Text;
+            String to = TxtDob2.Text;
+            displayData(from, to);
         }
 
         protected void Button2_Click(object sender, EventArgs e)
         {
+            ExportGridToExcel();
+        }
+        public override void VerifyRenderingInServerForm(Control control)
+        {
+            //required to avoid the runtime error "  
+            //Control 'GridView1' of type 'GridView' must be placed inside a form tag with runat=server."  
+        }
+        protected void displayData()
+        {
+            SqlCommand cmd = con.CreateCommand();
+            cmd.CommandType = CommandType.Text;
+            cmd.CommandText = "select RequestNumber, RequestDate, CreatedBy, TotalFare, RefernceNumber, AccountNumber, Action from Request";
+            cmd.ExecuteNonQuery();
+            DataTable dt = new DataTable();
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            da.Fill(dt);
+            GridView1.DataSource = dt;
+            GridView1.DataBind();
+        }
+
+        protected void displayData(String status)
+        {
+            SqlCommand cmd = con.CreateCommand();
+            cmd.CommandType = CommandType.Text;
+            cmd.CommandText = "select RequestNumber, RequestDate, CreatedBy, TotalFare, RefernceNumber, AccountNumber, Action from Request where Action = '" + status + "'";
+            cmd.ExecuteNonQuery();
+            DataTable dt = new DataTable();
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            da.Fill(dt);
+            GridView1.DataSource = dt;
+            GridView1.DataBind();
+        }
+
+        protected void displayData(String from, String to)
+        {
+            SqlCommand cmd = con.CreateCommand();
+            cmd.CommandType = CommandType.Text;
+            cmd.CommandText = "select RequestNumber, RequestDate, CreatedBy, TotalFare, RefernceNumber, AccountNumber, Action from Request where RequestDate BETWEEN '" + from + "'AND '" + to + "'";
+
+
+            cmd.ExecuteNonQuery();
+            DataTable dt = new DataTable();
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            da.Fill(dt);
+            GridView1.DataSource = dt;
+            GridView1.DataBind();
+        }
+
+
+        private void ExportGridToExcel()
+        {
+            Response.Clear();
+            Response.Buffer = true;
+            Response.ClearContent();
+            Response.ClearHeaders();
+            Response.Charset = "";
+            string FileName = "GR_REPORT_" + DateTime.Now + ".xls";
+            StringWriter strwritter = new StringWriter();
+            HtmlTextWriter htmltextwrtter = new HtmlTextWriter(strwritter);
+            Response.Cache.SetCacheability(HttpCacheability.NoCache);
+            Response.ContentType = "application/vnd.ms-excel";
+            Response.AddHeader("Content-Disposition", "attachment;filename=" + FileName);
+            GridView1.GridLines = GridLines.Both;
+            GridView1.HeaderStyle.Font.Bold = true;
+            GridView1.RenderControl(htmltextwrtter);
+            Response.Write(strwritter.ToString());
+            Response.End();
 
         }
+
     }
 }
